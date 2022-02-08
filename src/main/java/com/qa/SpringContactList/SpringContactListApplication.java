118c95b7614136a6e65a37ec80c02d4f4409bec1
FIRST_NAME LAST_NAME <My_NAME@example,com>
2022-02-08 10:55:19 +0000

    Initial Commit

---
+++ src/main/java/com/qa/SpringContactList/SpringContactListApplication.java
@@ -0,0 +1,13 @@
+package com.qa.SpringContactList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/SpringContactList/SpringContactListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.SpringContactList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContactListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
